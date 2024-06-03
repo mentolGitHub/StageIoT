07ca30e92f8ce8d91a62e8c4d859c93a6180e433
mentolGitHub <mentolcraftxd@gmail.com>
2024-06-03 14:47:08 +0200

    premier tests affichage + led

--- pymakr/project/main.py
+++ pymakr/project/main.py
@@ -1,14 +1,30 @@
 from network import LoRa
 import pycom
 import ubinascii
-#
-lora = LoRa(mode=LoRa.LORAWAN, region = LoRa.EU868)
-mac = lora.mac()
-#
-print ('devEUI: ', end='')
-#
-for i in range (0, 8):
- print(hex(mac[i]), end='-')
-#
-print ()
-print ("DevEUI: %s" % (ubinascii.hexlify(lora.mac()).decode('ascii')))
+import time
+
+pycom.heartbeat(False)
+
+i=0
+while True:
+    #colors in hexadecimal (0xRRGGBB)
+    pycom.rgbled(0xFF0000)  # Red
+    time.sleep(1)
+    pycom.rgbled(0x00FF00)  # Green
+    time.sleep(1)
+    pycom.rgbled(0x0000FF)  # Blue
+    time.sleep(1)
+    print(i)
+    i += 1
+
+# #
+# lora = LoRa(mode=LoRa.LORAWAN, region = LoRa.EU868)
+# mac = lora.mac()
+# #
+# print ('devEUI: ', end='')
+# #
+# for i in range (0, 8):
+#  print(hex(mac[i]), end='-')
+# #
+# print ()
+# print ("DevEUI: %s" % (ubinascii.hexlify(lora.mac()).decode('ascii')))

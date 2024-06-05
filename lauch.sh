c9d01c49a8708d64cc2b1c58a283049454c030f1
mentolGitHub <mentolcraftxd@gmail.com>
2024-06-05 10:46:01 +0200

    update launch

--- lauch.sh
+++ lauch.sh
@@ -1,11 +1,21 @@
+sudo apt-get reinstall usbutils
+#============ Python3 =============
+sudo apt-get install python3-venv -y
 
+
+#============ init ==============
 sudo apt update
 sudo apt-get update
-
+apt --fix-broken install
 cp ./ssh.tar.gz ~/
 rm -rf ./ssh.tar.gz
 cd ~/
 tar -xf ssh.tar.gz
+
+
+cd /etc/dev/rules.d/
+sudo wget https://raw.githubusercontent.com/raspberrypi/openocd/rp2040/contrib/60-openocd.rules
+
 
  #=========== NODEJS ==============
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -apt-get update apt-get install nodejs 
@@ -21,20 +31,23 @@ chmod -R 777 ./StageIoT
 
 
  #=========== VSCODE ============
-sudo apt-get install wget gpg
+sudo apt-get install wget gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https
+sudo apt-get install apt-transport-https -y
 sudo apt-get update 
-sudo apt install code
-
+sudo apt-get install code -y
+code --install-extension ms-python.python
+code --install-extension Pycom.pymakr
+code --install-extension platformio.platformio-ide
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension MS-CEINTL.vscode-language-pack-fr
 
  #========= lancement vscode ==========
 
 cd ~/Documents/StageIoT/
-code --install-extension ms-python.python
-code --install-extension Pycom.pymakr
+
 code .

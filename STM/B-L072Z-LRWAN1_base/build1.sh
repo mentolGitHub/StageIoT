da31aa8ae4692cd6d291cdf5d70f9b96b7fffad4
c-l-e-m-e-n-t <clement.cognard862002.cc@gmail.com>
2024-06-24 14:05:07 +0200

    delete les fichiers inutilisés

--- STM/B-L072Z-LRWAN1_base/build1.sh
+++
@@ -1,4 +0,0 @@
-set -x #echo on
-gprbuild default.gpr -v
-arm-eabi-objcopy -O binary -S bin/main.elf bin/main.bin
-cp bin/main.bin E:/
--- STM/B-L072Z-LRWAN1_base/test.gdb
+++
@@ -1,3 +0,0 @@
-target extended-remote localhost:4242
-file build/main.elf
-where

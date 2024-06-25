af2edb32bac9d7a3bf55588ff23198bcfb6412cb
Clément <59148218+c-l-e-m-e-n-t@users.noreply.github.com>
2024-06-26 00:03:23 +0200

    print uniquement si besoin uart pycom

--- pymakr/Uart/main.py
+++ pymakr/Uart/main.py
@@ -6,12 +6,12 @@ import time
 
 pycom.heartbeat(False)
 # this uses the UART_1 default pins for TXD and RXD (``P3`` and ``P4``)
-print("starting 1..")
-pycom.rgbled(0x010100)
+print("starting ...")
+pycom.rgbled(0x000100)
 uart = UART(1, baudrate=9600)
-buf = ""
 
 while 1 :
-    uart.write('test')   # write the 3 charactersà^)
-    print(uart.read(uart.any()))
+    #uart.write('test')   # write the 3 charactersà^)
+    if uart.any() > 0:
+        print(uart.read(uart.any()))
     time.sleep(2)

51bf0a5f241a801b6184adc05361971d59393901
mentolGitHub <mentolcraftxd@gmail.com>
2024-09-19 16:50:12 +0200

    avec stm

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 branch="TP1"
 
 #=========== NODEJS ==============
+sudo apt install python3-pip -y
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -apt-get update apt-get install nodejs 
 sudo apt-get update 
 sudo apt-get install nodejs -y
+
 
 #============= GIT =====================
 
@@ -29,3 +31,39 @@ code --install-extension MS-CEINTL.vscode-language-pack-fr
 
 #========= lancement vscode ==========
 code ./StageIoT-$branch
+
+
+#========== GDOWN =============
+
+pip install gdown
+export PATH=$PATH:/home/local/.local/bin
+echo "export PATH=$PATH:/home/local/.local/bin" >> ~/.bashrc
+#========= GetCubeIde ==========
+cd /home/local/Téléchargements/
+gdown https://drive.google.com/uc?id=1lUXRF5xAX0Q55UL_0ZMYPDgUpGt2td_x
+sudo mkdir /opt/st
+tar -xf stm32cubeide_1.15.1.tar.gz
+sudo cp -r ./stm32cubeide_1.15.1 /opt/st/
+sudo rm -rf ./stm32cubeide_1.15.1
+sudo rm -rf ./stm32cubeide_1.15.1.tar.gz
+sudo apt-get install libncurses5 -y
+#========= GetSTlink ============
+doc="st-stlink-server-2.1.1-1-linux-amd64.deb"
+cd /home/local/Téléchargements/
+gdown https://drive.google.com/uc?id=1W9A_pyXGvcN3RtvXQHMTdM7Nhi7Zc0Ws
+sudo dpkg -i st-stlink-server-2.1.1-1-linux-amd64.deb
+sudo rm -rf ./$doc
+#========= GetWorkspace ==========
+mkdir /home/local/STM32CubeIDE
+gdown https://drive.google.com/uc?id=1Z_aTaIkf1KT8YM6UOLyPlDdfHrfUaeOo
+tar -xf workspace_1.15.1.tar.gz
+
+sudo cp -r ./workspace_1.15.1 /home/local/STM32CubeIDE/
+sudo rm -rf ./workspace_1.15.1
+sudo rm -rf ./workspace_1.15.1.tar.gz
+sudo chmod  -R 777 /home/local/STM32CubeIDE/
+
+echo "alias cube-ide=\"/opt/st/stm32cubeide_1.15.1/stm32cubeide &\"" >> ~/.bashrc
+/opt/st/stm32cubeide_1.15.1/stm32cubeide &
+exec bash
+#========= fin ========
